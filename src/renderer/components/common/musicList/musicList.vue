15fa273ae31fd58878362467aeb27e0711e0f7d8
Astar <2320390964@qq.com>
2018-08-29 22:50:22 +0800

    歌曲列表静态页面

---
+++ src/renderer/components/common/musicList/musicList.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="music-list">
+        <music-list-item isheader />
+        <music-list-item v-for="(item, index) in musiclist" :item="item" :index="index" :key="item.id"/>
+    </div>
+</template>
+
+<script>
+import musicListItem from '../musicListItem/musicListItem'
+
+export default {
+    name: "music-list",
+    data () {
+        return {
+            musiclist:[
+                {id:'1', islike:true, name:'夏天要吃瓜', singer:'astar', album:'astar的夏天', source:'网易云', duration:'120s', playamount:100},
+                {id:'2', islike:false, name:'冬天穿长袖', singer:'astar', album:'astar的夏天', source:'网易云', duration:'120s', playamount:100},
+                {id:'4', islike:false, name:'冬天穿长袖', singer:'astar', album:'astar的夏天', source:'网易云', duration:'120s', playamount:100},
+                {id:'5', islike:false, name:'冬天穿长袖', singer:'astar', album:'astar的夏天', source:'网易云', duration:'120s', playamount:100},
+                {id:'6', islike:false, name:'冬天穿长袖', singer:'astar', album:'astar的夏天', source:'网易云', duration:'120s', playamount:100},
+                {id:'7', islike:false, name:'冬天穿长袖', singer:'astar', album:'astar的夏天', source:'网易云', duration:'120s', playamount:100},
+                {id:'8', islike:false, name:'冬天穿长袖', singer:'astar', album:'astar的夏天', source:'网易云', duration:'120s', playamount:100},
+                {id:'9', islike:false, name:'冬天穿长袖', singer:'astar', album:'astar的夏天', source:'网易云', duration:'120s', playamount:100},
+                {id:'10', islike:true, name:'冬天穿长袖', singer:'astar', album:'astar的夏天', source:'网易云', duration:'120s', playamount:100},
+                {id:'11', islike:true, name:'冬天穿长袖', singer:'astar', album:'astar的夏天', source:'网易云', duration:'120s', playamount:100},
+                {id:'12', islike:false, name:'冬天穿长袖', singer:'astar', album:'astar的夏天', source:'网易云', duration:'120s', playamount:100},
+                {id:'13', islike:false, name:'冬天穿长袖', singer:'astar', album:'astar的夏天', source:'网易云', duration:'120s', playamount:100},
+                {id:'14', islike:false, name:'冬天穿长袖', singer:'astar', album:'astar的夏天', source:'网易云', duration:'120s', playamount:100},
+                {id:'15', islike:false, name:'冬天穿长袖', singer:'astar', album:'astar的夏天', source:'网易云', duration:'120s', playamount:100},
+                {id:'16', islike:true, name:'冬天穿长袖', singer:'astar', album:'astar的夏天', source:'网易云', duration:'120s', playamount:100}
+            ]
+        }
+    },
+    components:{
+        musicListItem
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.music-list {
+    background-color:#fff;/* 明天装个sass-loader*/
+    width: 800px;
+    height:407px;
+    padding:0 29px;
+    overflow: hidden;
+}
+</style>
---
+++ src/renderer/components/common/musicListItem/musicListItem.vue
@@ -0,0 +1,173 @@
+<template>
+<div class="music-wrap">
+  <ul class="music-list-item" 
+    :class="{
+              'tbody':!isheader,
+              'tbody-gray':!isheader && isEven
+            }"
+  >
+    <template v-if="isheader">
+      <li class="t-index">
+        <span>
+          序号
+        </span>
+      </li>
+      <li class="t-song">
+        <span>
+          曲名
+        </span>
+      </li>
+      <li>
+        <span>
+          歌手名
+        </span>
+      </li>
+      <li>
+        <span>
+          专辑名
+        </span>
+      </li>
+      <li>
+        <span>
+          来源
+        </span>
+      </li>
+      <li>
+        <span>
+          歌曲时长 / 收听
+        </span>
+      </li>
+    </template>
+    <template v-else>
+      <li class="t-index">
+        <span>
+          {{itemindex}}
+        </span>
+        <img src="../assets/dislike.png" alt="dislike" v-if="!item.islike" class="heart">
+        <img src="../assets/like.png" alt="dislike" v-else class="heart">
+      </li>
+      <li class="t-song">
+        <span>
+          {{item.name}}
+        </span>
+      </li>
+      <li>
+        <span>
+          {{item.singer}}
+        </span>
+      </li>
+      <li>
+        <span>
+          {{item.album}}
+        </span>
+      </li>
+      <li>
+        <span>
+          {{item.source}}
+        </span>
+      </li>
+      <li>
+        <span>
+          {{item.duration}} / {{item.playamount}}
+        </span>
+      </li>
+    </template>
+  </ul>
+</div>
+</template>
+
+<script>
+export default {
+  name:'musicListItem',
+  props:{
+    isheader:{
+      type:Boolean,
+      default:false
+    },
+    item:{
+      type:Object,
+      default: function() {
+        return {}
+      }
+    },
+    index:{
+      type:Number,
+      default:0
+    }
+  },
+  computed:{
+    itemindex () {
+      return this.index < 10 ? '0'+this.index : ''+this.index
+    },
+    isEven () {
+      return !(this.index % 2)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.music-wrap {
+  .music-list-item {
+    margin: 0;
+    padding: 0;
+    height: 30px;
+    width: 800px;
+    li {
+      display:inline-block;
+      list-style:none;
+      width:100px;
+      height:12px;
+      font-size:8px;
+      line-height: 12px;
+      overflow: hidden;
+      padding: 9px 0;
+      text-align: left;
+      span {
+        color:#A0A0A0;
+        height:12px;
+      }
+    }
+    .t-index {
+      position: relative;
+      width:30px;
+      padding-left:15px;
+      margin-right:5px;
+      span{
+        border-left: none;
+      }
+      .heart {
+        position:absolute;
+        top: 10px;
+        right:0;
+        width:10px;
+      }
+    }
+    .t-song {
+      width: 350px;
+      span{
+        border-left: none;
+      }
+    }
+  }
+  .tbody {
+    border-left:1px solid #eee;
+    border-right: 1px solid #eee;
+    li {
+      span {
+        color:#000;
+        border-left:1px solid #AAAAAA;
+      }
+    }
+  }
+  .tbody-gray {
+    background-color: #eee;
+  }
+}
+
+
+
+
+</style>
+
+
